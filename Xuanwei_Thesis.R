39c310c11e81dfa68b8fc9e1136d68e6a9fc331a
KH <krishartin@gmail.com>
2015-05-14 18:58:45 -0700

    Start to merge R code

--- Xuanwei_Thesis.R
+++ Xuanwei_Thesis.R
@@ -1,41 +1,48 @@
-##Code that draws individual .csv files into one file
+# - - - - - - - - - - - - - - - - - - - - - #
+# Packages Needed to Complete this Analysis #
+# - - - - - - - - - - - - - - - - - - - - - #
 
-#Load the required packages - add the other packages for this analysis
+packages <- ("doBy", "psych", "beswarm")
 
-pkgs <- c("knitr", "XLConnect", "doBy", "beeswarm")
-for (pkg in pkgs) {
-  if (! require(pkg, character.only=T)) { 
-    install.packages(pkg, repos="http://cran.fhcrc.org", dependencies=TRUE)
-    suppressPackageStartupMessages(library(pkg))
-  }
-}
+#Import each origin .csv file excluding unnessesary rows and columns and export each as 'cleaned' dataset SECTION ON HOLD####
+##Something along the lines of Brian High's Example
 
-#Configure knitr options - From Brians Repo - Necessary?
+##Upload .CSV export csv with headers and units corrected SECTION ON HOILD####
+###IMport and change headers
+####From Surface
+NP <- read.csv("C:/Users/KGH/OneDrive/Documents/Xuanwei/Files/Results/NP/NPAH_Final_Summary.csv")
+names(NP) <- c("name","filterID", "sampletype", "np_2np_pg", "np _2nfl_pg", "np_1np_pg", "np_2np_flag",
+               "np_2nfl_flag", "np_1np_flag")
+names(NP)
+View(NP)
 
-opts_chunk$set(tidy=FALSE, cache=FALSE)
+####From UW
+NP_pg <- read.csv("C:/Users/khartin/Desktop/NPAH_Final_Summary.csv") #Temporary need to finalize
+names(NP) <- c("name","filterID", "sampletype", "np_2np_pg", "np _2nfl_pg", "np_1np_pg", "np_2np_flag",
+               "np_2nfl_flag", "np_1np_flag")
+names(NP)
+View(NP)
 
-#Create the data folder - probably don't need this if folder already exists
+NP_ug
 
-datadir <- "data"
-dir.create(file.path(datadir), showWarnings=FALSE, recursive=TRUE)
+###Correct COncentration
 
-#Identify Worksheet
+#Merge files into single dataset by filter ID
+##Not sure how yet, something like coursera example
 
-datafile <- 'Batch AA results.xlsx'
-datafileout <- paste(c(datadir, '/', datafile), sep='', collapse='')
 
-library(XLConnect)
-XRF <- readWorksheetFromFile(datafileout, sheet=1, header=TRUE,
-                             startRow=8, endRow=46)
+#Standardize units - CURRENTLY CALCULATED IN EXCEL BEFORE EXPOSRTING
+##Apply same folrmulae as in Excel workbook
 
-#Load dataset, exclude Xuanwei, view table
-#From UW
-Xuanwei_ForAnalysis_31Mar15 <- read.csv("H:/Seto_Projects/Xuanwei/Xuanwei_ForAnalysis_31Mar15.csv")
-X<-subset(Xuanwei_ForAnalysis_31Mar15, village != "Xuanwei")
-#detach(X)
-#attach(X)
+##Check mass balance
 
-###Data reduction
+###Produce Table and Plot
+
+#Data reduction
+
+##Average concentrations by site - Arithmetic mean
+
+
 
 ## Reduce number of metals to those with at least 10 greater than 2*Unc
 
@@ -47,16 +54,15 @@ v<-vector()
 for(i in 1:length(names(X))){
   if(grepl("Unc",names(X)[i])){
     if(sum(X[,i-1]>X[,i]*2)>10){
-    v<-c(v,paste(names(X)[i-1],"V",sep=""))
+      v<-c(v,paste(names(X)[i-1],"V",sep=""))
+    }
   }
-}
-}
-
+}  
 ##For names with "ConcV"
 #grepl("Conc",names(X))
 #var.names <- names(X)[grepl("ConcV",names(X))] ##Update column names
 
-#Creating file of Arithmetic Mean of values for each site
+#for loop AM of sites
 
 v<-c(v,names(X)[grepl("ugV",names(X))])
 
@@ -65,18 +71,45 @@ for (i in v){
   this.formula <- paste(i,this.formula, sep = "+")
 }
 
-am.site <- summaryBy(as.formula(this.formula), data=X, id = "village", keep.names=TRUE, FUN=mean)
+#Load dataset, exclude Xuanwei, view table - CURRENT WORKFLOW BELOW####
+##file_locationUW <- C("H:/Seto_Projects/Xuanwei/Xuanwei_ForAnalysis_31Mar15.csv") ##From UW
+file_locationPC <- c("C:/Users/KGH/OneDrive/Documents/Xuanwei/Xuanwei_ForAnalysis_31Mar15.csv") ##From Surface
+file_locationPC2 <- c("C:/Users/KGH/OneDrive/Documents/Xuanwei/Xuanwei_ForAnalysis_Test.csv") ##From Surface
+##Xuanwei_ForAnalysis_31Mar15 <- read.csv(file_locationUW)
+Xuanwei_ForAnalysis_31Mar15 <- read.csv(file_locationPC)
+X<-subset(Xuanwei_ForAnalysis_31Mar15, village != "Xuanwei")
+Xuanwei_ForAnalysis <- read.csv(file_locationPC2)
+X2<-subset(Xuanwei_ForAnalysis, village != "Xuanwei")
 
-##Create new objects -> Home, Ambient, Mean and Geometric Mean Home Concentrations 
+#Count number of observations per location type #Not sure if necessary anymore
+table(X$location)
+with(X,table(Concentration = conc,  sitenum))
 
-is.home <- subset(am.site, location == "Home")
-is.ambient <- subset(am.site, location == "Ambient")
+#Summary By ####
+install.packages("doBy")
+library(doBy)
+##Subset data by location and create mean of concentratins
+#Create new objects, one for Home and one for Ambient
+##Create new objects -> Home, Ambient, Mean and Geometric Mean Home Concentrations ####
+is.home <- subset(X, location == "Home")
+is.ambient <- subset(X, location == "Ambient")
+#home <- X[,"location"] #?
+#Xuanwei_Final [is.ambient,] #? Ditto
+with(subset(X, location == "Home" & sitenum == 1), summary(conc)) #summaryBy way easuer
+am.site <- summaryBy(conc ~ sitenum + location + village, data=X, id = NULL, keep.names=TRUE, FUN=mean)
+am.site
+summaryBy(conc ~ sitenum + location, data=X, id = NULL, keep.names=TRUE, FUN=mean)
+summaryBy(2NP_ugV ~ sitenum + location, data=X, id = NULL, keep.names=TRUE, FUN=mean)
+summaryBy( X2NP_ugV + X2NFL_ugV  +  X1NP_ugV  ~ sitenum + location, data=X2, id = NULL, keep.names=TRUE, FUN=mean, na.rm = TRUE)
+describeBy(X$conc, list(X$location, X$burn))
+describeBy(X$conc, list(X$location,X$sitenum))
+
 
 ##Plots of Conc by Site Differentiating Indoor and Outdoor 
 
-#Arithmetic Mean of Indoor and Dup Samples Showing Mean and Median of Indoor and Ambient 
+#Arithmetic Mean of Indoor and Dup Samples Showing Mean and Median of Indoor and Ambient ####
 
-plot(am.site$conc~am.site$sitenum,log="y",ylab="Concentration ug/m^3", xlab="Site Number",
+plot(conc~sitenum, type="n",log="y",ylab="Concentration ug/m^3", xlab="Site Number",
      main="PM2.5 Concentration by Site") #Why cannot control y range with ylim=c(0, 2000)?
 points(ho_avgmeans_x,col=1,pch=19)
 points(am_avgmeans_x,col=8,pch=15)
@@ -87,15 +120,36 @@ abline(h=median(ho_avgmeans_x), lty=5, col=1)
 abline(h=mean(am_avgmeans_x), lty=3, col=8)
 abline(h=median(am_avgmeans_x), lty=4, col=8)
 
+#Geometric Mean of Indoor and Dup Samples by Site Showing Geometric Mean and Median of Indoor and Ambient#### 
 
-#GM of Indoor and Dup Samples by Village Showing NAAQS 24hr (35 ug/m3)
+plot(conc~sitenum, type="n",log="y",ylab="Concentration ug/m^3", xlab="Site Number",
+     main="PM2.5 Concentration by Site") #Why cannot control y range with ylim=c(0, 2000)?
+points(ho_gmeans_x,col=1,pch=19)
+points(am_gmeans_x,col=8,pch=15)
+legend("topright", c("Indoor", "GM","Median","Ambient", "GM", "Median"),col=c(1,1,1,8,8,8),
+       pch=c(19,NA,NA,15,NA,NA),lty=c(NA,1,5,NA,3,4))
+abline(h=mean(ho_gmeans_x), lty=1, col=1)
+abline(h=median(ho_gmeans_x), lty=5, col=1)
+abline(h=mean(am_gmeans_x), lty=3, col=8)
+abline(h=median(am_gmeans_x), lty=4, col=8)
+
+#Geometric Mean of Indoor and Dup Samples by Site Showing NAAQS 24hr (35 ug/m3)####
+plot(conc~sitenum, type="n",log="y",ylab="Concentration ug/m^3", xlab="Site Number",
+     main="PM2.5 Concentration by Site") #Why cannot control y range with ylim=c(0, 2000)?
+points(ho_gmeans_x,col=1,pch=19)
+points(am_gmeans_x,col=8,pch=15)
+abline(h=35, lty=4, col=1)
+legend("topright", c("Indoor","Ambient","NAAQS 35 ug/m^3"),col=c(1,8,1),
+       pch=c(19,15,NA),lty=c(NA,NA,4))
+
+#GM of Indoor and Dup Samples by Village Showing NAAQS 24hr (35 ug/m3)####
 
 install.packages('beeswarm')
 library(beeswarm)
 
-boxplot(am.site$conc~am.site$village, pch=19, ylab="Concentration ug/m^3", main="Conc by Village", 
+boxplot(conc~village, data=gm_mean_site, pch=19, ylab="Concentration ug/m^3", main="Conc by Village", 
         log="y", ylim=c(10,1000))
-beeswarm(am.site$conc~am.site$village, pch=19, pwcol=ifelse(am.site$location=="Home",1,8), log="TRUE", 
+beeswarm(conc~village, data=gm_mean_site, pch=19, pwcol=ifelse(location=="Home",1,8), log="TRUE", 
          add=TRUE, ylim=c(10,1000))
 abline(h=35, lty=4, col=1)
 legend("topright", c("Indoor", "Ambient","NAAQS 35 ug/m^3"), col=c(1,8), pch=c(19,15,NA),lty=c(NA,NA,4))
@@ -109,7 +163,7 @@ ratio_site <- summaryBy(conc~location + sitenum, data=gm_site, id = "village", k
 #library(car)
 #scatterplot(conc~sitenum | location, data=gm_site)
 
-#Scatter plot of log indoor vs outdoor 
+#Scatter plot of log indoor vs outdoor ####
 
 lm_byloc <- lm(gm_site$conc[gm_site$location=="Ambient"]~gm_site$conc[gm_site$location=="Home"])
 plot(gm_site$conc[gm_site$location=="Home"], gm_site$conc[gm_site$location=="Ambient"], log = "xy", 
@@ -122,7 +176,7 @@ legend("topright", bty="O", legend=paste("R2 is",format(summary(lm_byloc)$adj.r.
 #lines(lowess(gm_site$conc[gm_site$location=="Home"], gm_site$conc[gm_site$location=="Ambient"]),
       col="blue") # lowess line (x,y)
 
-#Scatter plot of indoor vs outdoor 
+#Scatter plot of indoor vs outdoor ####
 
 plot(gm_site$conc[gm_site$location=="Home"], gm_site$conc[gm_site$location=="Ambient"], 
      main = expression(paste("Indoor vs Outdoor ",PM[2.5]," Conc", sep = "")),
@@ -131,31 +185,14 @@ plot(gm_site$conc[gm_site$location=="Home"], gm_site$conc[gm_site$location=="Amb
 abline(lm_byloc, col=1) # regression line (y~x) 
 legend("topright", bty="O", legend=paste("R2 is",format(summary(lm_byloc)$adj.r.squared, digits=4)))
 
-##Not sure if needeed####
+##Questionable code####
+#detach(X)
+#attach(X) #Try to refrain so no ambiguity or conflict after published
+#Log of Concentrations
+#logconc<-log(Xuanwei_Final$conc)
 #gm = function(x, na.rm=TRUE){
-#exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+  #exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
 #}
-
 #gm_site <- summaryBy(conc ~location + sitenum, data=X, id = "village", keep.names=TRUE, FUN=gm)
-
-#Geometric Mean of Indoor and Dup Samples by Site Showing Geometric Mean and Median of Indoor and Ambient
-
-#plot(conc~sitenum, type="n",log="y",ylab="Concentration ug/m^3", xlab="Site Number",
-#     main="PM2.5 Concentration by Site") #Why cannot control y range with ylim=c(0, 2000)?
-#points(ho_gmeans_x,col=1,pch=19)
-#points(am_gmeans_x,col=8,pch=15)
-#legend("topright", c("Indoor", "GM","Median","Ambient", "GM", "Median"),col=c(1,1,1,8,8,8),
-#       pch=c(19,NA,NA,15,NA,NA),lty=c(NA,1,5,NA,3,4))
-#abline(h=mean(ho_gmeans_x), lty=1, col=1)
-#abline(h=median(ho_gmeans_x), lty=5, col=1)
-#abline(h=mean(am_gmeans_x), lty=3, col=8)
-#abline(h=median(am_gmeans_x), lty=4, col=8)
-
-#Geometric Mean of Indoor and Dup Samples by Site Showing NAAQS 24hr (35 ug/m3)
-#plot(conc~sitenum, type="n",log="y",ylab="Concentration ug/m^3", xlab="Site Number",
-#     main="PM2.5 Concentration by Site") #Why cannot control y range with ylim=c(0, 2000)?
-#points(ho_gmeans_x,col=1,pch=19)
-#points(am_gmeans_x,col=8,pch=15)
-#abline(h=35, lty=4, col=1)
-#legend("topright", c("Indoor","Ambient","NAAQS 35 ug/m^3"),col=c(1,8,1),
-#       pch=c(19,15,NA),lty=c(NA,NA,4))
+#avgsiteconce_home<- subset(X,location=="Home" & avgsiteconc!="na")
+#avgsiteconce_ambient<- subset(Xuanwei_Final,village !="Xuanwei" & location=="Ambient" & avgsiteconc!="na")
